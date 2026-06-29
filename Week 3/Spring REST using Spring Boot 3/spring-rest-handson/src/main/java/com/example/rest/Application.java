4053ebc2a57b3bfb08fe34ad7dc9c61a44f7068c
SilentComet <fs885432@gmail.com>
2026-06-29 20:17:36 +0530

    Add files via upload

---
+++ Week 3/Spring REST using Spring Boot 3/spring-rest-handson/src/main/java/com/example/rest/Application.java
@@ -0,0 +1,13 @@
+package com.example.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource({"classpath*:country-beans.xml"})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}\n
---
+++ Week 3/Spring REST using Spring Boot 3/spring-rest-handson/src/main/java/com/example/rest/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World RESTful Web Service";
+    }
+}\n
